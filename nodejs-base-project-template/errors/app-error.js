6fdb5ced3397950075b9956d9f704408227dcfd9
AlicanBayir <42733073+AlicanBayir@users.noreply.github.com>
2019-04-02 12:20:48 +0300

    Update app-error.js

--- nodejs-base-project-template/errors/app-error.js
+++ nodejs-base-project-template/errors/app-error.js
@@ -3,15 +3,9 @@
 class AppError extends Error {
 
     constructor (code, message, status, data) {
-        // Calling parent constructor of base Error class.
         super(message);
-        // Saving class name in the property of our custom error as a shortcut.
         this.name = this.constructor.name;
-        // Capturing stack trace, excluding constructor call from it.
         Error.captureStackTrace(this, this.constructor);
-        // You can use any additional properties you want.
-        // I'm going to use preferred HTTP status for this error types.
-        // `500` is the default value if not specified.
         this.status = status || 500;
         this.setCode(code);
         this.setMessage(message);
